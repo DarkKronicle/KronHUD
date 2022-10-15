e82f0c125393e1d69ddb32295b9818c6d356fdca
georgijpronuk@gmail.com <georgijpronuk@gmail.com>
2022-10-15 11:43:50 +0300

    Use vehicle velocity in SpeedHud.java if player rides one

--- src/main/java/io/github/darkkronicle/kronhud/gui/hud/simple/SpeedHud.java
+++ src/main/java/io/github/darkkronicle/kronhud/gui/hud/simple/SpeedHud.java
@@ -4,12 +4,14 @@ import io.github.darkkronicle.kronhud.config.KronBoolean;
 import io.github.darkkronicle.kronhud.config.KronConfig;
 import io.github.darkkronicle.kronhud.gui.entry.SimpleTextHudEntry;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.List;
+import java.util.Objects;
 
 public class SpeedHud extends SimpleTextHudEntry {
 
@@ -24,7 +26,8 @@ public class SpeedHud extends SimpleTextHudEntry {
 
     @Override
     public String getValue() {
-        Vec3d vec = client.player.getVelocity();
+        ClientPlayerEntity player = Objects.requireNonNull(client.player);
+        Vec3d vec = player.getVehicle() == null ? player.getVelocity() : player.getVehicle().getVelocity();
         if (client.player.isOnGround() && vec.y < 0) {
             vec = new Vec3d(vec.x, 0, vec.z);
         }
